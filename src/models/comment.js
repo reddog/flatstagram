84825d3c932c952810f872411f47966067973e4f
David Bower <dave@koales.co.uk>
2018-06-09 15:05:26 +0100

    Comment class constructor and static all property

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,1 +1,23 @@
-// create Comment class here
+'use strict';
+// Comment model
+
+class Comment {
+  constructor(comment, imageId) {
+  	// ensure class static parameter all is an array, make it if not
+  	if(!Array.isArray(this.constructor.all)) {
+  		this.constructor.all = [];
+  	}
+
+  	// set instance variables
+	  this.id = this.constructor.all.length;
+	  this.image = this.findImage(imageId);
+	  this.commentContent = comment;
+
+	  // add this instance to list of all comments
+	  this.constructor.all.push(this);
+  }
+}
+
+// define array to hold all comments, a static propety of class Comment
+// can't declare static properties of classes in ES6
+Comment.all = [];
