8bf004d5c146e2d2f05fd725d85e95b5d82aae7d
David Bower <dave@koales.co.uk>
2018-06-09 11:20:14 +0100

    Iterate through each comment form and adds an eventlistener to trigger a function on form submit

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -5,9 +5,25 @@ class CommentsController {
 
   init() {
     // kick off controller from here
+
+    // add comment form event listeners
+    // this isn't *strictly* requested in the assignment
+    this.addCommentFormListener();
   }
 
   addCommentFormListener() {
-    // create comment form listener code here
+    // find all comment forms and add a submit listener
+    $('form.add-comment').on('submit', function(event){
+    	// event handler to handle comment form submit
+
+    	// prevent default form submit behaviour - we are going t handle the submit
+    	event.preventDefault();
+
+    	// make a jQuery object of the event so we can use jQuery helpers to get DOM element data
+    	var $event = $(event);
+
+    	// log the event to the console - just to show that something's working at this point
+    	console.log(event);
+    });
   }
 }
