1f116f676f4d7b84d70b70419e0a61c9a956d53a
David Bower <dave@koales.co.uk>
2018-06-09 15:16:40 +0100

    Add findImage method to find image by id and add current comment

--- src/models/comment.js
+++ src/models/comment.js
@@ -16,6 +16,34 @@ class Comment {
 	  // add this instance to list of all comments
 	  this.constructor.all.push(this);
   }
+
+  findImage(imageId) {
+  	// given an `int` for an image id, returns the image object with that id
+  	// before return - adds current comment to image's comments property
+  	// returns false on error or not found
+
+  	if(!Image || !Array.isArray(Image.all)) {
+  		return false;
+  	}
+
+		for(var image of Image.all) {
+			if(image.id == imageId) {
+				// found the matching image
+
+				// add this comment
+				if(!Array.isArray(image.comments)) {
+					image.comments = [];
+				}
+				image.comments.push(this);
+				
+				// return the image
+				return image;
+			}
+		}
+
+		// can't find the image for that id
+		return false;
+  }
 }
 
 // define array to hold all comments, a static propety of class Comment
