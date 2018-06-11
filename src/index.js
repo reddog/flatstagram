9666125adfaeab0911116f1c266714ef815d0a78
David Bower <dave@koales.co.uk>
2018-06-11 11:49:29 +0100

    Turn "test" flag to off for assessment submission

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 // flag to turn testing code on/off
 // set to true to include code not specifically requrested in the assessment instructions, but useful for testing
-const doTest = true;
+const doTest = false;
 
 $(function() { // on document ready
   imagesController = new ImagesController();
